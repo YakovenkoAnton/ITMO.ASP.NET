778e486126322480c2de838b8949cdd070b074af
Антон Яковенко <index812@yandex.ru>
2020-09-13 17:29:49 +0300

    executed

---
+++ ITMO.ASP.NET.MVC.Lab2/Models/ModelClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASP.NET.MVC.Lab2.Models
+{
+    public class ModelClass
+    {
+        public static string ModelHello()
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            return Greeting;
+        }
+    }
+
+    
+}   
