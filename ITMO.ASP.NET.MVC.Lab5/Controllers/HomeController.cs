45f49c5b4fb10fcbd16a11ba29f683c78ce73d6b
Антон Яковенко <index812@yandex.ru>
2020-09-13 23:41:56 +0300

    executed

--- ITMO.ASP.NET.MVC.Lab5/Controllers/HomeController.cs
+++ ITMO.ASP.NET.MVC.Lab5/Controllers/HomeController.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ITMO.ASP.NET.MVC.Lab5.Models;
 
 namespace ITMO.ASP.NET.MVC.Lab5.Controllers
 {
     public class HomeController : Controller
     {
+        private CreditContext db = new CreditContext();
+
         public ActionResult Index()
         {
+            var allCredits = db.Credits.ToList<Credit>();
+            ViewBag.Credits = allCredits;
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateBid()
+        {
+            GiveCredits(); 
+            var allBids = db.Bids.ToList<Bid>(); 
+            ViewBag.Bids = allBids; 
+            return View();
+        }
+
+        [HttpPost]
+        public string CreateBid(Bid newBid)
+        {
+            newBid.bidDate = DateTime.Now; 
+            // Добавляем новую заявку в БД 
+            db.Bids.Add(newBid); 
+            // Сохраняем в БД все изменения 
+            db.SaveChanges(); 
+            return "Спасибо, <b>" + newBid.Name + "</b>, за выбор нашего банка. Ваша заявка будет рассмотрена в течении 10 дней.";
+        }
+
+
+
+            private void GiveCredits()
+        {
+            var allCredits = db.Credits.ToList<Credit>();
+            ViewBag.Credits = allCredits;
         }
 
         public ActionResult About()
--- ITMO.ASP.NET.MVC.Lab5/Global.asax.cs
+++ ITMO.ASP.NET.MVC.Lab5/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using ITMO.ASP.NET.MVC.Lab5.Models;
 
 namespace ITMO.ASP.NET.MVC.Lab5
 {
@@ -12,6 +14,7 @@ namespace ITMO.ASP.NET.MVC.Lab5
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new CreditsDbInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
