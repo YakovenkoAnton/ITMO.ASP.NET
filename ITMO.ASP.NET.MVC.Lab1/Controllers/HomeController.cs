72fdc586537d2b3192f1f2a42503f8dea2345afe
Антон Яковенко <index812@yandex.ru>
2020-09-13 14:08:58 +0300

    Executed

---
+++ ITMO.ASP.NET.MVC.Lab1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ITMO.ASP.NET.MVC.Lab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITMO.ASP.NET.MVC.Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index(string hel) 
+        {
+            string Greeting = ModelClass.ModelHello() + ", " + hel; 
+            return Greeting; 
+        }
+
+
+    }
+}
---
+++ ITMO.ASP.NET.MVC.Lab1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITMO.ASP.NET.MVC.Lab1.MvcApplication" Language="C#" %>
